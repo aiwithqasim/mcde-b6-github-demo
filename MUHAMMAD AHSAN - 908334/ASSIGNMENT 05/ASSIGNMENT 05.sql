5bf4f2916fa467be82e782801088229bd308a05c
datawithahsan <313430443+muhammadahsansce1438-commits@users.noreply.github.com>
2026-09-20 07:51:58 +0500

    assignmwnt 5

--- MUHAMMAD AHSAN - 908334/ASSIGNMENT 05/ASSIGNMENT 05.sql
+++ MUHAMMAD AHSAN - 908334/ASSIGNMENT 05/ASSIGNMENT 05.sql
@@ -23,7 +23,7 @@ WHERE customer_id IN (
 
 
 -- 5.3
--- NOT IN NULL trap ko avoid karne ke liye NOT EXISTS use kareinn
+-- NOT IN NULL trap ko avoid karne ke liye NOT EXISTS use karein
 SELECT c.customer_id
 FROM sales.customers AS c
 WHERE NOT EXISTS (
